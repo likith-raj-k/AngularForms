eda3988da0caa1192ee96d3e720cd3801c1c27b7
Likith Raj K <rajlikith1998@gmail.com>
2023-08-02 10:55:32 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+  public onSubmit(theForm: NgForm) {
+      console.log(theForm.value.username);
+      console.log(theForm.value.password);
+  }
+}
+
+}
